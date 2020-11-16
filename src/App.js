c9ade89ebd412b9f0bf5f712e32f2e35e966bcd9
akine <weezer.and.you@gmail.com>
2020-11-16 22:00:26 +0900

    header

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
+import React from "react";
 import "./App.css";
+import Header from "./Header";
 
 function App() {
   return (
     // BEM class naming convention
     <div className="app">
-      <h1>build MERN TINDER clone</h1>
-
-      {/* Header */}
+      <Header />
       {/* TinderCards */}
       {/* SwipeButtons */}
     </div>
